cc47332f5b57054d4325bb1d811112e65556d0bf
otliq <otliq14@gmail.com>
2023-09-22 11:21:27 +0300

    Выполнение базовых требований

--- file_processing/core/models.py
+++ file_processing/core/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 class File(models.Model):
+    """
+    Представляеть загруженные файлы
+    Содержит поля:
+        file: поле типа FileField, используемое для загрузки файла.
+        uploaded_at: поле типа DateTimeField, содержит дату и время загрузки файла.
+        processed: поле типа BooleanField, указывает, был ли файл обработан.
+    """
     file = models.FileField()
     uploaded_at = models.DateTimeField(auto_now=True)
     processed = models.BooleanField(default=False)
